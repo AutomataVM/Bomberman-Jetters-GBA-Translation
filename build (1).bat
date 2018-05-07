a5103790c7cbe1a1ad8713885bc8afc063ecf3ea
Normmatt <normmatt234@gmail.com>
2018-05-07 15:33:23 +1200

    Disable debug output on atlas.

--- build (1).bat
+++ build (1).bat
@@ -4,15 +4,15 @@ armips.exe asm/Cheats.asm
 armips.exe asm/DebugMenu.asm
 armips.exe asm/graphicsChanges.asm
 armips.exe asm/ItemNamesVWF.asm
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_000.txt
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_001.txt
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_002.txt
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_003.txt
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_004.txt
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_005.txt
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_006.txt
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_007.txt
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_009.txt
-Atlas -d stdout rom\output.gba finalScript\bomberman_script_010.txt
+Atlas rom\output.gba script\bomberman_script_000.txt
+Atlas rom\output.gba script\bomberman_script_001.txt
+Atlas rom\output.gba script\bomberman_script_002.txt
+Atlas rom\output.gba script\bomberman_script_003.txt
+Atlas rom\output.gba script\bomberman_script_004.txt
+Atlas rom\output.gba script\bomberman_script_005.txt
+Atlas rom\output.gba script\bomberman_script_006.txt
+Atlas rom\output.gba script\bomberman_script_007.txt
+Atlas rom\output.gba script\bomberman_script_009.txt
+Atlas rom\output.gba script\bomberman_script_010.txt
 copy rom\output.gba rom\Bomberman_Jetters(E).gba
 pause
